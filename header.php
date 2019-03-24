adbf7f48126988d67a9a06b40e99b50758e17376
Anilmgr <mgranil60@gmail.com>
2019-03-24 14:26:35 +0545

    header commit

--- header.php
+++ header.php
@@ -7,5 +7,20 @@
 		<?php wp_head(); ?>
 	</head>
 <body <?php body_class(); ?>>
+	
+	<header>
+		<div class="container text-center">
+			<a class="brand-logo" href="<?php home_url('/');?>">Tashbir</a>			
+		</div>
+	</header>
 
-
+	<nav class="navbar navbar-expand-sm  sticky-top">
+		<ul class="navbar-nav mx-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Link</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Link</a>
+			</li>
+		</ul>
+	</nav>	
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 	get_header();	
 ?>
-
+	<section>
+		<div class="text-center">
+			
+		</div>
+	</section>
 
 <?php
 	get_footer();
